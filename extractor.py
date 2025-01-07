e90756980c1b12c2370cdaffe4df0f1f3e719c6c
RadunTheSniffer <ridhuan.work2001@gmail.com>
2025-01-07 17:31:44 +0800

    function creation

--- extractor.py
+++ extractor.py
@@ -1,16 +1,18 @@
 from bs4 import BeautifulSoup
 import requests 
+import main
 
-url = 'https://www.bbc.com/news'
-scraped_data = []
-response = requests.get(url)
-if response.status_code == 200:
-    #print(response.status_code)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    headlines = soup.find_all('h2')
-    for headline in headlines:
-        scraped_data.append(headline.text)
-      
-        
-else:
-    print(f"Failed to retireve the page. Status code : {response.status_code}")
+#url = 'https://www.bbc.com/news'
+def scrape_data(link):
+    scraped_data = []
+    response = requests.get(link)
+    if response.status_code == 200:
+        #print(response.status_code)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        headlines = soup.find_all('h2')
+        for headline in headlines:
+            scraped_data.append(headline.text)
+                    
+    else:
+        print(f"Failed to retireve the page. Status code : {response.status_code}")
+    return scraped_data
