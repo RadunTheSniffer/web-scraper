49c392fd10e8f77149e8bd19fdfc5c5a7cea8c3d
RadunTheSniffer <ridhuan.work2001@gmail.com>
2025-01-13 17:06:39 +0800

    fixed request error

--- extractor.py
+++ extractor.py
@@ -2,14 +2,15 @@ from bs4 import BeautifulSoup
 import requests 
 import main
 
-#url = 'https://www.bbc.com/news'
+#url = 'https://www.bbc.com/news'              manual test
 def scrape_data(link):
     scraped_data = []
     response = requests.get(link)
     if response.status_code == 200:
-        #print(response.status_code)
+        #print(response.status_code)             # debugging statement
         soup = BeautifulSoup(response.content, 'html.parser')
         headlines = soup.find_all('h2')
+        print(headlines)
         for headline in headlines:
             scraped_data.append(headline.text)
                     
