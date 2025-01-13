3dc070353d15d084bc242176f3f89d55238520b1
RadunTheSniffer <ridhuan.work2001@gmail.com>
2025-01-13 17:06:56 +0800

    preprocessing function addition

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template, request, jsonify
 import sentiment
+import classification
+import extractor
+
 app = Flask(__name__)
 
+def preprocess_data(link):
+    # Add your data cleaning and preprocessing steps here
+    cleaned_data = extractor.scrape_data(link)
+    return cleaned_data
 
 @app.route('/scraper', methods=['POST'])
 def scraper():
     data = request.get_json()
     link = data.get('link')
-    result = sentiment.analyze(link)
-    return jsonify(result) 
     
+    # Preprocess the data
+    cleaned_data = extractor.scrape_data(link)
+    
+    # Classification
+    classify = classification.filter_redundant_texts(cleaned_data)
+    
+    # Sentiment Analysis
+    result = sentiment.analyze(classify)
+    
+    return jsonify(result)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
